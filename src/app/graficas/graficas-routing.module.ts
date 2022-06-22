171bd70203974d11cbd570d814e14559929bf99c
Nicolas Castro Giraldo <nicolas.castro9219@gmail.com>
2022-06-22 17:26:01 -0500

    componente de barras funcional con el menu

--- src/app/graficas/graficas-routing.module.ts
+++ src/app/graficas/graficas-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BarrasComponent } from './pages/barras/barras.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    children:[
+      {path: 'barra', component: BarrasComponent},
+      {path:'**',redirectTo:'barra'}
+    ]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/shared/menu/menu.component.ts
+++ src/app/shared/menu/menu.component.ts
@@ -1,16 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+interface MenuItem {
+  ruta: string;
+  texto: string
+}
+
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styles: [
+  styles: [`
+  li{
+    cursor: pointer;
+  }
+  `
   ]
 })
-export class MenuComponent implements OnInit {
+export class MenuComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  menu: MenuItem[] = [
+    {ruta: '/graficas/barra', texto: 'Barras'}
+  ];
 
 }
